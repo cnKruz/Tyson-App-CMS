012116ce8ba2caa7fe419f3f1262972a133c90a9
cnKruz <cnKruz@users.noreply.github.com>
2015-02-27 19:26:52 -0600

    created service file
    created service file

---
+++ PHP/service.php
@@ -0,0 +1,13 @@
+<?php
+include_once("producto.class.php");
+
+	//$mysqli = new mysqli("ejemplo.com", "usuario", "contraseña", "basedatos");
+	$server="162.144.32.74";
+	$user="";
+	$pass="";
+	$database="";
+	$mysqli=new mysqli($server, $user, $pass, $database);
+
+
+
+?>
